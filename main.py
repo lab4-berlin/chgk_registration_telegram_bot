f6a23310a0008c07dc20ae34da77ea41e18af7d9
lab4-berlin <ilya@lab4.berlin>
2025-02-03 11:39:12 +0100

    Update main.py - new async processing in main

--- main.py
+++ main.py
@@ -105,4 +105,9 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        print("Bot stopped manually.")
